2bcde78d7d4958cec917834275792409030d5381
Daniel Segal <dlemel8@gmail.com>
2020-08-14 18:51:40 +0300

    done bouncy until 2.13.7

---
+++ bouncy/src/main.rs
@@ -0,0 +1,83 @@
+enum VerticalDirection {
+    Up,
+    Down,
+}
+
+enum HorizontalDirection {
+    Left,
+    Right,
+}
+
+struct Ball {
+    vertical_position: u32,
+    horizontal_position: u32,
+    vertical_direction: VerticalDirection,
+    horizontal_direction: HorizontalDirection,
+}
+
+impl Ball {
+    fn bounce(&mut self, frame: &Frame) {
+        if self.horizontal_position == 0 {
+            self.horizontal_direction = HorizontalDirection::Right
+        }
+
+        if self.horizontal_position == frame.width - 1 {
+            self.horizontal_direction = HorizontalDirection::Left
+        }
+
+        if self.vertical_position == 0 {
+            self.vertical_direction = VerticalDirection::Up
+        }
+
+        if self.vertical_position == frame.height - 1 {
+            self.vertical_direction = VerticalDirection::Down
+        }
+    }
+
+    fn move_(&mut self) {
+        match self.vertical_direction {
+            VerticalDirection::Down => self.vertical_position -= 1,
+            VerticalDirection::Up => self.vertical_position += 1,
+        }
+
+        match self.horizontal_direction {
+            HorizontalDirection::Left => self.vertical_position -= 1,
+            HorizontalDirection::Right => self.vertical_position += 1,
+        }
+    }
+}
+
+struct Frame {
+    width: u32,
+    height: u32,
+}
+
+struct Game {
+    frame: Frame,
+    ball: Ball,
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {
+            frame: Frame {
+                width: 60,
+                height: 30,
+            },
+            ball: Ball {
+                vertical_position: 2,
+                horizontal_position: 4,
+                vertical_direction: VerticalDirection::Up,
+                horizontal_direction: HorizontalDirection::Left,
+            },
+        }
+    }
+
+    fn step(&mut self) {
+        self.ball.bounce(&self.frame);
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
