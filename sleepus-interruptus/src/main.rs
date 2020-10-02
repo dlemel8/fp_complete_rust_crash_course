5d6d45a17c64ae044f321aed72e7c85034a4e35d
Daniel Segal <dlemel8@gmail.com>
2020-10-02 17:58:55 +0300

    implement sleepus-interruptus until 8.10

--- sleepus-interruptus/src/main.rs
+++ sleepus-interruptus/src/main.rs
@@ -1,10 +1,78 @@
-use async_std::task::{sleep, spawn};
+use std::future::Future;
+use std::pin::Pin;
 use std::time::Duration;
 
-async fn sleepus() {
-    for i in 1..=10 {
-        println!("sleepus {}", i);
-        sleep(Duration::from_millis(500)).await;
+use async_std::task::{Context, Poll, sleep, spawn};
+use pin_project_lite::pin_project;
+
+struct SleepPrint<Fut> {
+    sleep: Fut,
+}
+
+impl<Fut: Future<Output=()>> Future for SleepPrint<Fut> {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let sleep: Pin<&mut Fut> =
+            unsafe { self.map_unchecked_mut(|s| &mut s.sleep) };
+
+        match sleep.poll(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(()) => {
+                println!("Inside SleepPrint");
+                Poll::Ready(())
+            }
+        }
+    }
+}
+
+pin_project! {
+    struct TwoFutures<Fut1, Fut2>{
+        first_done: bool,
+        #[pin]
+        first: Fut1,
+        #[pin]
+        second: Fut2,
+    }
+}
+
+impl<Fut1: Future, Fut2: Future> Future for TwoFutures<Fut1, Fut2> {
+    type Output = Fut2::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        if !*this.first_done {
+            if let Poll::Ready(_) = this.first.poll(cx) {
+                *this.first_done = true;
+            }
+        }
+
+        if *this.first_done {
+            this.second.poll(cx)
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+// async fn sleepus() {
+fn sleepus() -> impl Future<Output=()> {
+    // for i in 1..=10 {
+    //     println!("sleepus {}", i);
+    //     sleep(Duration::from_millis(500)).await;
+    // }
+
+    // SleepPrint {
+    //     sleep: sleep(Duration::from_secs(3))
+    // }
+
+    TwoFutures {
+        first_done: false,
+        first: sleep(Duration::from_secs(3)),
+        second: {
+            println!("Hello TwoFutures");
+            async_std::future::ready(())
+        },
     }
 }
 
